2373baf13307c778baa7420d9f81c6f46895b3d0
Eric Tornabell Noguera <etornabell@idiapjgol.info>
2022-12-19 10:17:05 +0100

    petits ajustaments 19/12/22

--- tractament_dades_cat.R
+++ tractament_dades_cat.R
@@ -5,7 +5,8 @@ library(dplyr)
 poblacional <- read_excel("~idiap/data/ABS_CanGibert_MartaFigueras_Odontologia/Catalunya/dades_poblacionals.xlsx")
 salutotal <- read_excel("~idiap/data/ABS_CanGibert_MartaFigueras_Odontologia/Catalunya/salut_oral.xlsx")
 accessibilitat <- read_excel("~idiap/data/ABS_CanGibert_MartaFigueras_Odontologia/Catalunya/accessibilitat.xlsx")
-correlacio <- read_excel("~idiap/data/ABS_CanGibert_MartaFigueras_Odontologia/Catalunya/dades_correlacio.xlsx")
+regio <- read_excel("~idiap/data/ABS_CanGibert_MartaFigueras_Odontologia/Catalunya/dades_correlacio.xlsx")
+correlacio <- read_excel('~idiap/data/ABS_CanGibert_MartaFigueras_Odontologia/Desigualtats salut oral CATnomsOK.xlsx')
 
 pob <- poblacional[complete.cases(poblacional),]
 salut <- salutotal[complete.cases(salutotal),]
@@ -14,8 +15,11 @@ acces[acces == 'no'] <- NA
 
 cat <- left_join(pob, salut)
 cat <- left_join(cat, acces)
+regions <- cat %>% select(ABS, Regió)
+
 
 corr <- correlacio[complete.cases(correlacio),]
 corr[corr=='no']<-NA
+corr <- left_join(corr, regions)
 save(cat, file = '~idiap/projects/dadesRuth/dadescat.RData')
 save(corr, file = '~idiap/projects/dadesRuth/corrcat.RData')
